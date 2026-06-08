6a6e963e1cb827ff7eb3120075f0cefe3acdb244
ksandra-bat <a.lavler@ya.ru>
2026-06-08 16:14:28 +0200

    Добавлен файл requirements.txt и функция подписи автора в файле main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello from repository!')
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из локального файла .env
+load_dotenv()
+
+def print_author():
+	author=os.getenv("AUTHOR")
+	print(f"Автор проекта: {author}")
+
+if __name__=="__main__":
+	print_author()
